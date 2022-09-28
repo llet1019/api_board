7ca379433efe2edbadacc1a9da9f5902be0809f7
llet1019 <llet1019@gmail.com>
2022-09-28 15:33:59 +0900

    Delete seret

--- community/community/settings.py
+++ community/community/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mkn0)(_y7u34v=t+b)nkk%er(me&ez5$rjw0^a7eo*glkw(+_i'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
