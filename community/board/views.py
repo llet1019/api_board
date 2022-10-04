f99aa2f41bebf47c17d7e6b3915e1820c341b0c5
llet1019 <llet1019@gmail.com>
2022-10-04 21:18:04 +0900

    Add board list api

--- community/board/views.py
+++ community/board/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .serializer import BoardSerializer
-from .models import Board
+from .models import Board, Category
 from user.models import User
 
 
@@ -61,6 +61,62 @@ class BoardViewSets(viewsets.ModelViewSet):
                         'data': {},
                         'message': '수정에 실패하였습니다.'
                     }
+        except Exception as e:
+            result_data = {
+                'code': 501,
+                'data': {},
+                'message': f'{str(e)} 오류가 발생했습니다.'
+            }
+        return Response(result_data)
+
+    @action(detail=True, methods=['get'])
+    def get_info(self, request, *args, **kwargs):
+        try:
+            board = Board.objects.get(id=kwargs['board_id'])
+            serializer = BoardSerializer(board)
+            result_data = {
+                'code': 200,
+                'data': serializer.data,
+                'message': '게시물 조회에 성공하였습니다.'
+            }
+        except Exception as e:
+            result_data = {
+                'code': 501,
+                'data': {},
+                'message': f'{str(e)} 오류가 발생했습니다.'
+            }
+        except Board.DoesNotExist:
+            result_data = {
+                'code': 404,
+                'data': {},
+                'message': '존재하지 않는 게시물입니다.'
+            }
+        return Response(result_data)
+
+    @action(detail=True, methods=['get'])
+    def list(self, request, *args, **kwargs):
+        try:
+            boards = Board.objects.filter(category_id=kwargs['category_id'])
+            if boards.count() == 0:
+                if Category.objects.filter(id=kwargs['category_id']).exists():
+                    result_data = {
+                        'code': 200,
+                        'data': {},
+                        'message': f'게시물이 존재하지 않습니다.'
+                    }
+                else:
+                    result_data = {
+                        'code': 404,
+                        'data': {},
+                        'message': f'존재하지 않는 카테고리입니다.'
+                    }
+            else:
+                serializer = BoardSerializer(boards, many=True)
+                result_data = {
+                    'code': 200,
+                    'data': serializer.data,
+                    'message': f'게시판 조회에 성공하였습니다.'
+                }
         except Exception as e:
             result_data = {
                 'code': 501,
